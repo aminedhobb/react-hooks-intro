8510e6e8207bdb39378c7f514bf18715be5582b2
aminedhobb <amine.dhobb@gmail.com>
2020-01-07 17:27:46 +0100

    use previous state

--- src/AppFunction.jsx
+++ src/AppFunction.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const App = () => {
   const [count, setCount] = useState(0)
 
-  const incrementCount = () => setCount(count + 1)
+  const incrementCount = () => setCount(prevCount => prevCount + 1)
   return(
     <button onClick={incrementCount}>I was clicked {count} times</button>
   )
