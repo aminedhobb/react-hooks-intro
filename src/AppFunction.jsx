ce327ac3fcf7a6b013292d9842e7bd01b4a5dd7c
aminedhobb <amine.dhobb@gmail.com>
2020-01-07 19:15:44 +0100

    other types of listeners

--- src/AppFunction.jsx
+++ src/AppFunction.jsx
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from 'react'
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  state: null
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
   const [isOn, setIsOn] = useState(false)
   const [mousePosition, setMousePosition] = useState({x: null, y: null})
+  const [status, setStatus] = useState(navigator.onLine)
+  const [{latitude, longitude, speed}, setLocation] = useState(initialLocationState)
+  let mounted = true
 
   useEffect(() => {
     document.title = `You have clicked ${count} times`
     window.addEventListener('mousemove', handleMouseMove)
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
+    navigator.geolocation.getCurrentPosition(handleGeolocation)
+    const watchId = navigator.geolocation.watchPosition(handleGeolocation)
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove)
+      window.removeEventListener('online', handleOnline)
+      window.removeEventListener('offline', handleOffline)
+      navigator.geolocation.clearWatch(watchId)
+      mounted = false
     }
   }, [count])
 
@@ -20,6 +37,19 @@ const App = () => {
       y: event.pageY
     })
   }
+
+  const handleGeolocation = (event) => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      })
+    }
+  }
+
+  const handleOnline = () => setStatus(true)
+  const handleOffline = () => setStatus(false)
 
   const incrementCount = () => setCount(prevCount => prevCount + 1)
   const toggleLight = () => setIsOn(!isOn)
@@ -44,6 +74,13 @@ const App = () => {
       />
       <h2> Mouse Position </h2>
       {JSON.stringify(mousePosition, null, 2)}
+      <br />
+      <h2> Network Status </h2>
+      <p> You are <strong>{status ? "online" : "offline"}</strong></p>
+      <h2> Geolocation </h2>
+      <p> latitude is {latitude} </p>
+      <p> longitude is {longitude} </p>
+      <p> speed is {speed ? speed : "0" } </p>
     </>
   )
 }
