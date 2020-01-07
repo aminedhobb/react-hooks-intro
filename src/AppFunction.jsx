1d1f0114fabd5add45fd0198f4e6a441bcf51c8c
aminedhobb <amine.dhobb@gmail.com>
2020-01-07 17:52:02 +0100

    toggle state with useState

--- src/AppFunction.jsx
+++ src/AppFunction.jsx
@@ -2,10 +2,30 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [isOn, setIsOn] = useState(false)
 
   const incrementCount = () => setCount(prevCount => prevCount + 1)
+  const toggleLight = () => setIsOn(!isOn)
+
   return(
-    <button onClick={incrementCount}>I was clicked {count} times</button>
+    <>
+      <h2> button </h2>
+      <button onClick={incrementCount}>I was clicked {count} times </button>
+      <h2> light </h2>
+      <img
+        onClick={toggleLight}
+        src={
+          isOn
+          ? "https://icon.now.sh/highlight/fd0"
+          : "https://icon.now.sh/highlight/aaa"
+        }
+        style={{
+          height: "50px",
+          width: "50px",
+        }}
+        alt = "light"
+      />
+    </>
   )
 }
 
